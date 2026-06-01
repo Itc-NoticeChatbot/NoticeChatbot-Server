de4b797b241721f79f166a3aded0bcea234961b7
huniversal <hunivers0523@naver.com>
2026-06-01 18:10:10 +0900

    chore: initialize spring boot server scaffold

---
+++ src/main/java/com/inhatc/noticebot/NoticeServerApplication.java
@@ -0,0 +1,13 @@
+package com.inhatc.noticebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeServerApplication.class, args);
+	}
+
+}
